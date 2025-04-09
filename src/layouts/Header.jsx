627987d363851caebde184cccc3c4e4682aee054
coderaao <akbarovazizbek5560@gmail.com>
2025-04-09 23:41:30 +0500

    add header component with navigation and icons

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,10 +1,49 @@
 import React from "react";
+import hamburger from "../assets/images/hamburger-menu.svg";
+import github from "../assets/images/github.svg";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
-      <header>
-        <div className="bg-[#1B1B1B] h-12 w-full text-white">Header</div>
+      <header className="bg-[#1B1B1B] py-2 px-3.5">
+        <div className="container w-full h-12 mx-auto flex justify-between items-center">
+          <div className="flex items-center gap-x-3.5">
+            <button className="p-1 border-none cursor-pointer">
+              <img src={hamburger} alt="Menu Icon" />
+            </button>
+            <NavLink
+              to="/"
+              className="text-[22px] leading-7 font-medium text-white"
+            >
+              AZIZBEK<span className="text-[#39965F]">.UZ</span>
+            </NavLink>
+          </div>
+          <nav className="flex items-center gap-x-3.5">
+            <ul className="flex items-center gap-x-6 text-white text-base font-normal">
+              <li>
+                <NavLink to="/">Bosh sahifa</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">Haqida</NavLink>
+              </li>
+              <li>
+                <NavLink to="/projects">Loyihalar</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contacts">Bog'lanish</NavLink>
+              </li>
+            </ul>
+            <NavLink
+              to="https://github.com/programmingWithAzizbek"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="p-1 inline-block"
+            >
+              <img src={github} alt="Github Icon" />
+            </NavLink>
+          </nav>
+        </div>
       </header>
     </>
   );
